ab21ef92b9a5968f7b49127bb106c5ad71a52fb8
ChallengerSong <37797147+ChallengerSong@users.noreply.github.com>
2021-06-17 16:53:03 +0800

    Update app.py

--- python-flask/app.py
+++ python-flask/app.py
@@ -24,4 +24,4 @@ def web_invoke():
     return "Hello from SCF HTTP function, your input: " + event_str + ", request_id: " + request_id + "\n"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=9000)
+    app.run(host='0.0.0.0', port=9000)
